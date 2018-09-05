add3bd8a794cf03661bdb3839fac5fdf27232531
Brittney <brittfort34@gmail.com>
2018-09-05 06:41:34 +0000

    Level 2

--- node.js
+++ node.js
@@ -1,11 +1,8 @@
-var http = require('http'); 
+var EventEmitter = require('events').EventEmitter;
 
-http.createServer(function(request, response) {
-   response.writeHead(200); 
-   response.write("Dog is running.");
-   setTimeout(function(){
-       response.write("Dog is done.")
-       response.end();
-   }, 5000);
-}).listen(8080);
-console.log('Listening on port 8080...');
+var logger = new EventEmitter();
+logger.on('error', function(message){
+   console.log('ERR: ' + message); 
+});
+
+logger.emit('error', 'Spilled Milk');
